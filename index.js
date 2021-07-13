b60c0661973e653d6b5114c4aa7541e24117687d
kashif-raza2019 <kashifraza08012001@gmail.com>
2021-07-14 00:45:08 +0530

    Uploaded Files

---
+++ index.js
@@ -0,0 +1,18 @@
+var gitLink = document.getElementById("gitHub");
+var instaLink = document.getElementById("insta");
+var linkedInLink = document.getElementById("linkedIn");
+var movies = document.getElementById("movies");
+gitLink.onclick = function(){
+    window.location.href='http://www.github.com/kashif-raza2019';
+}
+instaLink.onclick = function(){
+    window.location.href='http://www.instagram.com/kashifrazareal';
+}
+linkedInLink.onclick = function(){
+    window.location.href='https://www.linkedin.com/in/kashif-raza-994232189/';
+}
+
+movies.onclick = function(){
+    window.location.href='movies.html';
+}
+
